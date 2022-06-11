7a0023c431ba88c455c7b43f60e7a19189ab49a5
Score2 <836696023@qq.com>
2022-06-11 19:58:38 +0800

    init

---
+++ src/main/kotlin/com/iroselle/minecraft/skycolorful/SkyColorful.kt
@@ -0,0 +1,11 @@
+package com.iroselle.minecraft.skycolorful
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object SkyColorful : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running ExamplePlugin!")
+    }
+}
